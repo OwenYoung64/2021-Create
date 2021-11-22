6368feadbbeffee44726d7dcb2f6e97bc9523f44
Owen Young <owen.young@vtsd.com>
2021-11-22 12:29:11 -0500

    commit

---
+++ main.py
@@ -0,0 +1,44 @@
+#2021 AP Creat Task
+#Imports
+import turtle as trtl
+
+#Setup
+wn = trtl.Screen()
+wn.bgcolor("black")
+startgame = trtl.Turtle()
+startgame.shape("circle")
+startgame.fillcolor("green")
+startgame.turtlesize(5)
+
+startgame.write("Start")
+
+#Functions
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#-----function calls-----
+wn.listen()
+wn.mainloop()
