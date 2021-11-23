3c2ac007182df15f859028aeb42031ae7d1cc318
Owen Young <owen.young@vtsd.com>
2021-11-23 11:45:22 -0500

    commit

--- main.py
+++ main.py
@@ -2,37 +2,43 @@
 #Imports
 import turtle as trtl
 
-#Setup
+#Variables
 wn = trtl.Screen()
-wn.bgcolor("black")
 startgame = trtl.Turtle()
-startgame.shape("circle")
+start_text = trtl.Turtle()
+greenball = trtl.Turtle()
+redball = trtl.Turtle()
+fontsetup = ("Arial", 15, "normal")
+#Variable setup
+#Setup wn
+wn.bgcolor("black")
+#Setup startgame
+startgame.hideturtle()
+startgame.penup()
+startgame.shape("square")
 startgame.fillcolor("green")
 startgame.turtlesize(5)
+#Setup start_text
+start_text.hideturtle()
+start_text.penup()
+start_text.backward(20)
+start_text.right(90)
+start_text.forward(10)
+#start_text.write("Start", font=fontsetup)
+#Setup greenball
+greenball.shape("circle")
+greenball.fillcolor("green")
+greenball.turtlesize(5)
 
-startgame.write("Start")
+#Setup redball
+redball.shape("circle")
+redball.fillcolor("red")
+redball.turtlesize(5)
+redball.forward(100)
 
 #Functions
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def draw_the_balls():
 
 
 
@@ -40,5 +46,6 @@ startgame.write("Start")
 
 
 #-----function calls-----
+#startgame.onclick()
 wn.listen()
 wn.mainloop()
